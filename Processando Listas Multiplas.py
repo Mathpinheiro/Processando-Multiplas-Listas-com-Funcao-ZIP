c51c8022c3f9303e315b8669b831ae95540418a6
Matheus Pinheiro <pinheiro.matheus.93@gmail.com>
2023-03-26 18:18:03 -0300

    commit inicial

---
+++ Processando Listas Multiplas.py
@@ -0,0 +1,8 @@
+from itertools import zip_longest
+
+produtos = ['Produto 1', 'Produto 2', 'Produto 3', 'Produto 4', 'Produto 5']
+
+precos = ['R$500,00', 'R$1500,00', 'R$2700,00', 'R$5000,00']
+
+for produtos,precos in zip_longest(produtos, precos):
+   print(f'{produtos} encontrado no valor de {precos}')
